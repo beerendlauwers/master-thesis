72860907e1b95f15f77c8e80f6a355befb7fa741
frank <frank@50b2131d-b0ec-4b63-a1bd-122eb01deaf5>
2009-11-16 21:18:13 +0000

    dit de pagina die een nieuwe gebruiker in de asp.net tabbellen steekt ipv oude tabbellen en profiles gebruikt in web.config

---
+++ ASP.Net-project/Project/cars/App_Presentation/Webpaginas/nieuwe gebruiker.aspx.vb
@@ -0,0 +1,25 @@
+﻿
+Partial Class App_Presentation_Webpaginas_nieuwe_gebruiker
+    Inherits System.Web.UI.Page
+
+    Protected Sub CreateUserWizard1_CreatedUser(ByVal sender As Object, ByVal e As System.EventArgs) Handles CreateUserWizard1.CreatedUser
+        ' create an empty profile
+
+        Dim p As ProfileCommon = ProfileCommon.Create(CreateUserWizard1.UserName, True)
+
+        ' fill profile with values from CreateUserStep
+
+
+        p.voornaam = CType(CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtVoornaam"), TextBox).Text
+        p.naam = CType(CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtNaam"), TextBox).Text
+        p.geboortedatum = CType(CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtgeboorte"), TextBox).Text
+        p.identiteitsnummer = CType(CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtIdentiteitsNr"), TextBox).Text
+        p.rijbewijsnummer = CType(CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtRijbewijsNr"), TextBox).Text
+        p.telefoon = CType(CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtTelefoon"), TextBox).Text
+        p.Email = CType(CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtEmail"), TextBox).Text
+        p.BTWnummer = CType(CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("txtBTW"), TextBox).Text
+
+        ' save profile
+
+    End Sub
+End Class
