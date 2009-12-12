f94a24ac0b32cf0b1982232725feba4f6cc65db4
frederik <frederik@50b2131d-b0ec-4b63-a1bd-122eb01deaf5>
2009-12-12 16:30:05 +0000

    * Naar mijn weten zou alles moeten werken eens een bepaalde bug wordt gefixed. Ik ga er nu zelf nog wat verder op zoeken, maar moest iemand degelijke input hebben in dit probleem gelieve het te delen:
    Ik kan dus geen UserID aanspreken binnen BLLReservatie, maar alle andere kolommen wel. Heeft er volgens mij iets met te maken dat dit zo'n "26ed26d4f51f4f5645f" waarde bevat...

--- ASP.Net-project/Project/cars/App_Presentation/Webpaginas/Beheer/AutoInchecken.aspx
+++ ASP.Net-project/Project/cars/App_Presentation/Webpaginas/Beheer/AutoInchecken.aspx
@@ -9,63 +9,79 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="plcMain" runat="Server">
   
-Kenteken:
-         
-                <asp:DropDownList ID="ddlKenteken" runat="server">
-                </asp:DropDownList>
-      
-          
-                <br />
-                <br />
-                ReservatieID:
-                <asp:TextBox ID="txtResID" runat="server"></asp:TextBox>
-      
-          
-                <br />
-                Datum:
-          
-                <asp:TextBox ID="txtDatum" runat="server"></asp:TextBox>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+        
+        <table>
+        <tr>
+            <td>
+            ReservatieID:
+            </td>
+            <td>
+            <asp:TextBox ID="txtResID" runat="server" AutoPostBack="True"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            Datum:
+            </td>
+            <td>
+                </calendarextender>
+                <asp:TextBox ID="txtDatum" runat="server" AutoPostBack="True"></asp:TextBox>
                 <cc1:CalendarExtender ID="calDatum" runat="server" 
-                TargetControlID="txtDatum" Format="d/MM/yyyy" FirstDayOfWeek="Monday" 
-                TodaysDateFormat="d MMMM, yyyy" PopupButtonID="imgDatumKalender"></cc1:CalendarExtender>
-            <asp:Image ImageAlign="AbsMiddle" ID="imgDatumKalender" runat="server" ImageUrl="~/App_Presentation/Images/kalender.png" /> 
+                    TargetControlID="txtDatum" Format="d/MM/yyyy" FirstDayOfWeek="Monday" 
+                    TodaysDateFormat="d MMMM, yyyy" 
+                    PopupButtonID="imgDatumKalender">
                 </cc1:CalendarExtender>
-                
-                 
-                 <br />
-                <br />
-                
-                 
-                 Boete:<br />
-&nbsp;<asp:TextBox ID="txtBoete" runat="server"></asp:TextBox>
-    
-                <br />
-                <br />
-                Problematisch:<br />
-&nbsp;<asp:TextBox ID="txtProblematisch" runat="server" Height="96px" Width="323px"></asp:TextBox>
-                 
-                 
-
-    
-
- 
-
-                
-        
-     
-
-                <br />
-                <br />
+                <asp:Image ID="imgDatumKalender" runat="server" ImageAlign="AbsMiddle" 
+                    ImageUrl="~/App_Presentation/Images/kalender.png" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Boete:
+            </td>
+            <td>
+                <asp:TextBox ID="txtBoete" runat="server"></asp:TextBox>
+           </td>
+        </tr>
+        <tr>
+            <td>
+                 Problematisch:  
+            </td>
+            <td>
+                 <asp:CheckBox ID="chkProblematisch" runat="server" />  
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                Commentaar:<br />
+                <asp:TextBox ID="txtCommentaar" runat="server" Height="96px" Width="323px"></asp:TextBox>
+            </td>      
+        </tr>
+        <tr>
+            <td>
                 <asp:Button ID="cmdCheckIn" runat="server" Text="Check in" />
-                 
-                 
-
-    
-
- 
-
                 
-        
-     
+            </td>
+            <td>
+            
+                <asp:Label ID="lblResultaat" runat="server" Text=""></asp:Label>
+                <asp:UpdateProgress ID="UpdateProgress1" runat="server">
+                    <ProgressTemplate>
+                        <div class="progress">
+                            <img src="../../Images/ajax-loader.gif" /> Even wachten aub...
+                        </div>
+                    </ProgressTemplate>
+                </asp:UpdateProgress>
+            </td>
+        </tr>
+            </table>
+            
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+                  
+                 
 
 </asp:Content>
--- ASP.Net-project/Project/cars/App_Presentation/Webpaginas/Beheer/AutoInchecken.aspx.vb
+++ ASP.Net-project/Project/cars/App_Presentation/Webpaginas/Beheer/AutoInchecken.aspx.vb
@@ -3,19 +3,117 @@ Partial Class App_Presentation_Webpaginas_Beheer_AutoInchecken
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Dim autobll As New AutoBLL
-        Dim kentekens() As String = autobll.GetAllKentekens()
+        'Dim autobll As New AutoBLL
+        'Dim kentekens() As String = autobll.GetAllKentekens()
 
-        For Each waarde In kentekens
-            Me.ddlKenteken.Items.Add(waarde)
-        Next
+        'For Each waarde In kentekens
+        '    Me.ddlKenteken.Items.Add(waarde)
+        'Next
 
-        txtDatum.Text = DateTime.Now.Date()
+        If Not IsPostBack Then
+            txtDatum.Text = DateTime.Now.Date()
+            'txtBoete.Enabled = False
+            chkProblematisch.Checked = False
+        End If
+
+        
+
 
 
     End Sub
 
     Protected Sub cmdCheckIn_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmdCheckIn.Click
+
+        Try
+
+            
+
+            ' Reservatie
+            Dim BLLReservatie As New ReservatieBLL
+            Dim DTReservatie As Reservaties.tblReservatieDataTable
+            Dim DRReservatie As Reservaties.tblReservatieRow
+
+            DTReservatie = BLLReservatie.GetReservatieByReservatieID(txtResID.Text)
+            DRReservatie = DTReservatie.Rows(0)
+
+            ' Auto
+            Dim BLLAuto As New AutoBLL
+            Dim DTAuto As Autos.tblAutoDataTable
+            Dim DRAuto As Autos.tblAutoRow
+
+            DTAuto = BLLAuto.GetAutoByAutoID(DRReservatie.autoID)
+            DRAuto = DTAuto.Rows(0)
+
+            ' Klant
+            Dim BLLKlant As New KlantBLL
+            Dim DTKlant As New Klanten.tblUserProfielDataTable
+            Dim DRKlant As Klanten.tblUserProfielRow
+
+            '   Hij kan user ID niet inlezen van DRReservatie(verkloot de hele sub)
+            DTKlant = BLLKlant.GetUserProfielByUserID(DRReservatie.userID)
+            DRKlant = DTKlant.Rows(0)
+
+
+
+            ' Instellen Gegevens
+
+            ' Reservatie
+            ' Reservatiestatus 4 betekent: uitgecheckt en opnieuw ingecheckt
+            DRReservatie.reservatieStatus = 4
+            DRReservatie.reservatieIngechecktDoorMedewerker = New Guid(Membership.GetUser(User.Identity.Name).ProviderUserKey.ToString())
+
+            ' Auto
+            DRAuto.statusID = 5 'Vrij(?)
+
+            ' Klant
+
+
+
+            If chkProblematisch.Checked = True Then
+                DRKlant.userIsProblematisch = 1
+            Else
+                DRKlant.userIsProblematisch = 0
+            End If
+
+            DRKlant.userCommentaar = txtCommentaar.Text
+
+            ' Wegschrijven gegevens
+
+            If (BLLReservatie.UpdateReservatie(DRReservatie)) Then
+                'Gelukt!
+
+
+
+                If (BLLAuto.UpdateAuto(DRAuto)) Then
+                    'Gelukt!
+                    lblResultaat.Text = "De auto werd succesvol ingecheckt."
+                Else
+                    lblResultaat.Text = "Er was een probleem bij het aanpassen van de autogegevens. Contacteer de systeembeheerder."
+                End If
+
+                If (BLLKlant.UpdateUserProfiel(DRKlant)) Then
+                    'Gelukt!
+                    lblResultaat.Text = "De auto werd succesvol ingecheckt."
+                Else
+                    lblResultaat.Text = "Er was een probleem bij het aanpassen van de klantgegevens. Contacteer de systeembeheerder."
+                End If
+
+            Else
+                lblResultaat.Text = "Er was een probleem bij het aanpassen van de reservatiegegevens. Contacteer de systeembeheerder."
+            End If
+
+            Me.lblResultaat.Visible = True
+
+        Catch ex As Exception
+            Throw ex
+        End Try
+
+
+    End Sub
+
+    Private Sub AlterFields()
+        ' Boete bepalen
+
 
         Dim BLLReservatie As New ReservatieBLL
         Dim DTReservatie As Reservaties.tblReservatieDataTable
@@ -31,16 +129,51 @@ Partial Class App_Presentation_Webpaginas_Beheer_AutoInchecken
         DTAuto = BLLAuto.GetAutoByAutoID(DRReservatie.autoID)
         DRAuto = DTAuto.Rows(0)
 
-        Dim DatNu, DatRes As Date
-        
+
+        ' Berekent aantal dagen te laat
 
         Dim ts As TimeSpan = CDate(txtDatum.Text).Subtract(DRReservatie.reservatieEinddat)
 
         If ts.Days <= 0 Then
             txtBoete.Enabled = False
+            'MsgBox("ts.Days <= 0")
         Else
             txtBoete.Text = ts.Days * DRAuto.autoDagTarief
+            'MsgBox("ts.Days > 0")
         End If
 
+
+
+        ' Commentaar bij klant invullen
+
+
+        Dim BLLKlant As New KlantBLL
+        Dim DTKlant As New Klanten.tblUserProfielDataTable
+        Dim DRKlant As Klanten.tblUserProfielRow
+
+
+        'Hij kan user ID niet inlezen van DRReservatie(verkloot de hele sub)
+        DTKlant = BLLKlant.GetUserProfielByUserID(DRReservatie.userID)
+        DRKlant = DTKlant.Rows(0)
+
+        'commentaar uitlezen
+        txtCommentaar.Text = DRKlant.userCommentaar
+
+        'checkbox problematisch instellen
+
+        ' blijkbaar is het veld "UserIsProblematisch een int ipv boolean
+        ' Ik ga er van uit dat er dus nog andere bedoelingen bij zijn...
+        If DRKlant.userIsProblematisch > 0 Then
+            chkProblematisch.Checked = True
+        End If
+
+    End Sub
+
+    Protected Sub txtResID_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtResID.TextChanged
+        AlterFields()
+    End Sub
+
+    Protected Sub txtDatum_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtDatum.TextChanged
+        AlterFields()
     End Sub
 End Class
