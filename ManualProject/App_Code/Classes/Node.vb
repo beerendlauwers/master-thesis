6ea2bac49e3bcb0777d4350887768f7e4c65884a
beerend <beerend@50b2131d-b0ec-4b63-a1bd-122eb01deaf5>
2010-03-12 13:54:40 +0000

    Toegevoegd: Trees worden nu éénmaal ingeladen bij de eerste page request.

--- ManualProject/App_Code/Classes/Node.vb
+++ ManualProject/App_Code/Classes/Node.vb
@@ -20,19 +20,53 @@ Public Class Node
         _children = New List(Of Node)
     End Sub
 
+    Public Property ID() As Integer
+        Get
+            Return _ID
+        End Get
+        Set(ByVal value As Integer)
+            _ID = value
+        End Set
+    End Property
+
+    Public Property Type() As ContentType
+        Get
+            Return _type
+        End Get
+        Set(ByVal value As ContentType)
+            _type = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' Voeg een kind aan deze node toe.
+    ''' </summary>
     Public Sub AddChild(ByRef node As Node)
         _children.Add(node)
     End Sub
 
+    ''' <summary>
+    ''' Geeft het aantal kinderen van deze nodes terug.
+    ''' </summary>
     Public Function GetChildCount() As Integer
         Return _children.Count
     End Function
 
+    ''' <summary>
+    ''' Doorzoek recursief de kinderen van deze node, de kinderen van de kinderen, etc.
+    ''' </summary>
     Public Function GetChildBy(ByVal id As Integer, ByVal type As ContentType) As Node
 
         For Each n As Node In _children
+            'Huidige kinderen doorzoeken
             If n._ID = id And n._type = type Then
                 Return n
+            End If
+
+            'Kinderen van dit kind doorzoeken
+            Dim child As Node = n.GetChildBy(id, type)
+            If child IsNot Nothing Then
+                Return child
             End If
         Next n
 
--- ManualProject/App_Code/Classes/Tree.vb
+++ ManualProject/App_Code/Classes/Tree.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic
+Imports Manual
 
 Public Class Tree
     Private _naam As String
@@ -25,6 +26,18 @@ Public Class Tree
             _rootnode = node
         End Set
     End Property
+
+    ''' <summary>
+    ''' Doorzoek recursief de tree om een node te vinden op bassi van id en type.
+    ''' </summary>
+    Public Function DoorzoekTreeVoorNode(ByVal id As Integer, ByVal type As ContentType) As Node
+
+        If (_rootnode.ID = id And _rootnode.Type = ContentType.Categorie) Then
+            Return _rootnode
+        End If
+
+        Return _rootnode.GetChildBy(id, type)
+    End Function
 
     ''' <summary>
     ''' Haal een tree op op basis van de unieke combinatie van taal, versie en het bedrijf.
@@ -54,20 +67,88 @@ Public Class Tree
     End Function
 
     ''' <summary>
-    ''' Voeg een tree toe 
+    ''' Voeg een tree toe.
     ''' </summary>
-    Public Shared WriteOnly Property AddTree() As Tree
-        Set(ByVal tree As Tree)
+    Public Shared Sub AddTree(ByVal tree As Tree)
+        'Als de treelijst niet bestaat, maken we deze aan
+        If (FTrees Is Nothing) Then
+            FTrees = New List(Of Tree)
+        End If
 
-            'Als de treelijst niet bestaat, maken we deze aan
-            If (FTrees Is Nothing) Then
-                FTrees = New List(Of Tree)
-            End If
+        'Als de tree niet bestaat, voegen we hem toe
+        If (GetTree(tree) Is Nothing) Then
+            FTrees.Add(tree)
+        End If
+    End Sub
 
-            'Als de tree niet bestaat, voegen we hem toe
-            If (GetTree(tree) Is Nothing) Then
-                FTrees.Add(tree)
-            End If
-        End Set
-    End Property
+    ''' <summary>
+    ''' Vraag de verzameling trees op.
+    ''' </summary>
+    Public Shared Function GetTrees() As List(Of Tree)
+        Return FTrees
+    End Function
+
+    Public Shared Sub BouwTrees()
+        'Trees opbouwen
+        Dim dblink As DatabaseLink = DatabaseLink.GetInstance
+
+        Dim dbcategorie As CategorieDAL = dblink.GetCategorieFuncties
+        Dim dbtaal As TaalDAL = dblink.GetTaalFuncties
+        Dim dbbedrijf As BedrijfDAL = dblink.GetBedrijfFuncties
+        Dim dbversie As VersieDAL = dblink.GetVersieFuncties
+
+        'Alle talen ophalen
+        Dim taaldt As tblTaalDataTable = dbtaal.GetAllTaal
+        Dim bedrijfdt As tblBedrijfDataTable = dbbedrijf.GetAllBedrijf
+        Dim versiedt As tblVersieDataTable = dbversie.GetAllVersie
+        Dim categoriedt As tblCategorieDataTable
+
+        For Each taal As tblTaalRow In taaldt
+            For Each bedrijf As tblBedrijfRow In bedrijfdt
+                For Each versie As tblVersieRow In versiedt
+
+                    categoriedt = dbcategorie.GetAllCategorieBy(taal.TaalID, bedrijf.BedrijfID, versie.VersieID)
+
+                    'Root node ophalen en aanmaken
+                    Dim rootnode As Node
+                    Dim rootnoderij As tblCategorieRow = categoriedt.Rows(0)
+                    If rootnoderij.Categorie = "root_node" Then
+                        rootnode = New Node(rootnoderij.CategorieID, ContentType.Categorie, String.Empty, bedrijf.Naam)
+                    Else
+                        MsgBox("Er is een fout gebeurd tijdens het genereren van de categoriestructuren: De basis (of root node) van de boomstructuur bestaat niet.")
+                        Return
+                    End If
+
+                    Dim treenaam As String = String.Concat("TREE_", taal.Taal, "_", bedrijf.Naam, "_", versie.Versie)
+                    Dim t As New Tree(treenaam, taal.TaalID, versie.VersieID, bedrijf.BedrijfID, rootnode)
+                    Dim huidigeParent As Node
+
+                    For Each categorie As tblCategorieRow In categoriedt
+
+                        'De root node heeft geen parent, dus in dit geval kunnen we gewoon verdergaan.
+                        If categorie.FK_parent = categorie.CategorieID Then Continue For
+
+                        'Haal de juiste parent op van dit kind.
+                        huidigeParent = t.DoorzoekTreeVoorNode(categorie.FK_parent, ContentType.Categorie)
+
+                        'Check of huidigeParent geldig is
+                        If (huidigeParent Is Nothing) Then
+                            MsgBox(String.Concat("Er is een fout gebeurd tijdens het genereren van de categoriestructuren: De parent met nummer ", categorie.FK_parent, " bestaat niet."))
+                            Return
+                        End If
+
+                        'Maak een kind aan
+                        Dim kind As New Node(categorie.CategorieID, ContentType.Categorie, "", categorie.Categorie)
+
+                        'Plaats het kind onder de parent
+                        huidigeParent.AddChild(kind)
+
+                    Next categorie
+
+                    Tree.AddTree(t)
+
+                Next versie
+            Next bedrijf
+        Next taal
+    End Sub
 End Class
