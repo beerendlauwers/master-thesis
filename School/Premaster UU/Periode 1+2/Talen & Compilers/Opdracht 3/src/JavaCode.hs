d10fa268ddf25f6ee028c4413238006d9d257883
beerend <beerend@50b2131d-b0ec-4b63-a1bd-122eb01deaf5>
2011-01-24 12:58:51 +0000

    * Environment geïmplementeerd met de hulp van Dennis en Rutger

--- School/Premaster UU/Periode 1+2/Talen & Compilers/Opdracht 3/src/JavaCode.hs
+++ School/Premaster UU/Periode 1+2/Talen & Compilers/Opdracht 3/src/JavaCode.hs
@@ -1,20 +1,28 @@
 module JavaCode where
 
 import Prelude hiding (LT, GT, EQ)
-import Data.Map as M
+import Data.Map as M hiding (map,filter,lookup)
 import JavaLex
 import JavaGram
 import JavaAlgebra
 import SSM
 import Data.Char
 
+data VariableType = Local | Argument deriving (Show,Eq)
+type VarData = (Int,VariableType)
+type Env = [(String,VarData)] -- [(Variabelenaam, (Plaats t.o.v. MP, Type))]
+type EnvVars = Env -> (Code, [String])
+
+--type Env = Map String VarData -- Mapt een variabelenaam naar zijn data
+
+
 data ValueOrAddress = Value | Address
   deriving Show
 
 codeAlgebra :: JavaAlgebra Code 
                            Code 
-                           Code 
-                           (ValueOrAddress -> Code)
+                           EnvVars 
+                           (ValueOrAddress -> Env -> Code)
 
 codeAlgebra = ( (fClas)
               , (fMembDecl,fMembMeth)
@@ -25,46 +33,83 @@ codeAlgebra = ( (fClas)
  fClas       c ms     = [Bsr "main", HALT] ++ concat ms
 
  fMembDecl   d        = []
+ getVarName (Decl _ (LowerId name) ) = name
+ cleanUpFunction n = let minusN = negate n  -- Gekregen van Rutger
+                     in [ UNLINK, STS minusN, AJS (minusN+1), RET]
  fMembMeth   t m ps s = case m of
-                          LowerId x -> [LABEL x] ++ s ++ [RET]
+                          LowerId x -> let argumentLength = length ps
+                                           offsetBy val = 0 + (3 - argumentLength + val)
+                                           toData decl pos = (getVarName decl, (offsetBy pos, Argument))
+                                           argList = zipWith toData ps [1..argumentLength] -- Maak lijst van argumenten
+                                           --argMap = map (\(k,v) -> map insert k v) argList -- Steek ze in een map
+                                           toLocal name pos = (name, (pos, Local))
+                                           localsList list = zipWith toLocal list [1..localsLength]
+                                           (body, localVars) = s $ (localsList localVars) ++ argList
+                                           localsLength = length localVars
+                                        in [LABEL x, LINK localsLength, LDR MP] ++ body ++ cleanUpFunction argumentLength
                         
- fStatDecl   d        = []
- fStatExpr   e        = e Value ++ [pop]
- fStatIf     e s1 s2  = let c  = e Value
-                            n1 = codeSize s1
-                            n2 = codeSize s2
-                        in  c ++ [BRF (n1 + 2)] ++ s1 ++ [BRA n2] ++ s2
- fStatWhile  e s1     = let c = e Value
-                            n = codeSize s1
-                            k = codeSize c
-                        in  [BRA n] ++ s1 ++ c ++ [BRT (-(n + k + 2))] 
- fStatReturn e        = e Value ++ [STR R3] ++ [RET]
- fStatBlock  ss       = concat ss
+ fStatDecl   d        env = ([], [getVarName d]) -- Geen code, wel lokale variabele gedeclareerd
  
- fExprCon    c        va = case c of
-                             ConstInt n -> [LDC n]
-                             ConstBool b -> let boolean = if b == True then 1 else 0
-                                            in [LDC boolean]
-                             ConstChar x -> [LDC (ord x)]
- fExprVar    v        va = case v of
-                             LowerId x -> let loc = 37
-                                          in  case va of
-                                                Value    ->  [LDL  loc]
-                                                Address  ->  [LDLA loc] 
- fExprOp     o e1 e2  va = case o of
-                             Operator "=" -> e2 Value ++ [LDS 0] ++ e1 Address  ++ [STA 0]
-                             Operator (x:"=") -> let isAssignOp = elem x assignOps
-                                                     actualOp = (:[]) x
-                                                  in if isAssignOp == True
-                                                      then e2 Value ++ e1 Value ++ [opCodes ! actualOp] ++ e1 Address ++ [STA 0]
-                                                      else otherOps (x:"=")
-                             Operator op  -> otherOps op
-                             where otherOps op = e1 Value ++ e2 Value ++ [opCodes ! op]
+ fStatExpr   e        env = (e Value env ++ [pop], []) -- 1 stukje code, geen declaratie
+ 
+ fStatIf     e s1 s2  env = let c  = e Value env
+                                (thenCode,thenVars) = s1 env
+                                (elseCode,elseVars) = s2 env
+                                n1 = codeSize thenCode
+                                n2 = codeSize elseCode
+                                allCode = c ++ [BRF (n1 + 2)] ++ thenCode ++ [BRA n2] ++ elseCode
+                                allVars = thenVars ++ elseVars
+                            in (allCode,allVars)
+                            
+ fStatWhile  e s1     env = let c = e Value env
+                                (loopCode,loopVars) = s1 env
+                                n = codeSize loopCode
+                                k = codeSize c
+                                allCode = [BRA n] ++ loopCode ++ c ++ [BRT (-(n + k + 2))] 
+                            in (allCode,loopVars)
+                            
+ fStatReturn e        env = let isArgument x = case x of
+                                                (_,(_,Argument)) -> True
+                                                otherwise        -> False
+                                argumentLength = length $ filter isArgument env
+                                allCode = e Value env ++ [STR R3] ++ cleanUpFunction argumentLength
+                            in (allCode, [])
+
+ fStatBlock  ss      env = foldl merge ([],[]) $ map (\f -> f env) ss -- Gekregen van Rutger
+                           where merge (a,b) (c,d) = (a++c,b++d)
+ 
+ fExprCon    c        va env = case c of
+                                ConstInt n -> [LDC n]
+                                ConstBool b -> let boolean = if b == True then 1 else 0
+                                               in [LDC boolean]
+                                ConstChar x -> [LDC (ord x)]
+ fExprVar    v        va env = case v of
+                                LowerId x -> let (pos,vartype) = env ? x
+                                             in  case va of
+                                                  Value    ->  [LDL  pos]
+                                                  Address  ->  [LDLA pos] 
+ fExprOp     o e1 e2  va env = case o of
+                                Operator "=" -> e2 Value env ++ [LDS 0] ++ e1 Address env  ++ [STA 0]
+                                Operator (x:"=") -> let isAssignOp = elem x assignOps
+                                                        actualOp = [x]
+                                                    in if isAssignOp == True
+                                                        then e2 Value env ++ e1 Value env ++ [opCodes ! actualOp] ++ [LDS 0] ++ e1 Address env ++ [STA 0]
+                                                        else otherOps (x:"=")
+                                Operator op  -> otherOps op
+                                where otherOps op = e1 Value env ++ e2 Value env ++ [opCodes ! op]
                              
- fExprMethod na vars  va = case na of
-                            LowerId name -> concat [x va | x <- vars] ++ [LDL 0, Bsr name, LDR R3]
+ fExprMethod na vars  va env = case na of
+                                LowerId name -> concat [x va env | x <- vars] ++ [LDL 0, Bsr name, LDR R3]
 
 assignOps = ['+','-','/','*','%','$']
+
+simply :: Maybe a -> a
+simply (Just x) = x
+simply Nothing = error "Item not found!"
+
+(?) :: Eq a => [(a,b)] -> a -> b
+((a,b):ts) ? x | x==a      = b
+               | otherwise = ts ? x
                               
 opCodes :: Map String Instr
 opCodes
