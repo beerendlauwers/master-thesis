51dbe92510cc506e8f002104587000f576dde082
beerend <beerend@50b2131d-b0ec-4b63-a1bd-122eb01deaf5>
2013-04-13 19:54:40 +0000

    * reeds wat werk om functiedefinities te transformeren naar lambda + case, moet nog wat preprocessing doen van de patterns

--- master-thesis/contract-inferencing-helium/ContractInferencing/SyntaxTransformations.hs
+++ master-thesis/contract-inferencing-helium/ContractInferencing/SyntaxTransformations.hs
@@ -41,3 +41,33 @@ expandLets (LetR [] e _) = e
 transformLets = U.transform f
   where f (LetR dl e r) = expandLets (LetR dl e r)
         f x = x
+
+--Simplifies function definitions into lambda definitions.
+transformFunctions :: Domain.FP.Syntax.Decl -> Domain.FP.Syntax.Decl
+transformFunctions (DFunBinds decls) = 
+ let allPats = collectPatterns decls
+     collectPatterns = map (\(FunBind _ _ ps _) -> ps)
+     collectRhs = map (\(FunBind _ _ _ rhs) -> rhs)
+     allRhs = collectRhs decls
+     differentPats = filter (\x -> length x == 1) allPats
+     allVars = ["p" ++ show i | i <- [1..length (head allPats)]]
+     allMatches = map (\x -> Var $ Ident x) allVars
+     allPatVars = map (\x -> PVar $ Ident x) allVars
+     funcIdentifier = PVar $ (\(FunBind _ f _ _) -> f) (head decls)
+ in DPatBind funcIdentifier (Rhs (Paren $ Lambda allPatVars (constructCase allMatches (zip [1..] allPats) (zip [1..] allRhs))) [])
+
+-- TODO: Preprocess patterns so they're all the same.
+-- Currently, x and y are different patterns...
+constructCase :: [Expr] -> [(Int,Pats)] -> [(Int,Rhs)] -> Expr
+constructCase [m] pats rhs =  
+ let alts = map (\(n,[p]) -> (n,p)) pats
+     altExprs = map (\(n,pat) -> Alt Nothing pat (fromJust $ lookup n rhs)) alts 
+ in Case m altExprs
+constructCase (m:ms) pats rhs = 
+ let alts = head $ map nub $ transpose (map snd pats)
+     nextVals = groupBy (\(_,x) (_,y) -> head x == head y) pats
+     valsByAlt = concatMap (map  ( \(n,(x:xs)) -> (x,(n,xs)) )) nextVals
+     selectValsByAlt alt = map snd $ filter (\(x,_) -> x == alt) valsByAlt 
+     --valsByAlt = concatMap (\(x:xs) -> zip x xs) $ map (map nub) (map transpose $ map snd nextVals)
+     altExprs = map (\pat -> Alt Nothing pat (Rhs (constructCase ms (selectValsByAlt pat) rhs) [])) alts
+ in Case m altExprs
