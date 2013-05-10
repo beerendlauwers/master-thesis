fffffc10b6613c3b1fb366bebfed7a0c5aadb71d
beerend <beerend@50b2131d-b0ec-4b63-a1bd-122eb01deaf5>
2013-05-10 16:41:26 +0000

    * Prettyprinting toegevoegd voor nog enkele andere AST nodes. * Types worden nu gegenereerd voor elke functie- en let-definitie. * Debug-info uitgebreid zodat je ook de eigenlijk code kan zien, alsook de types. * Compilatiecode verplaatst naar Views.ag * Types kunnen worden omgezet naar contracten. Moet nog gecheckt worden of dit correct is! * Substitutiecode van Jurriën gedeeltelijk overgebracht.
    Nog te doen: In het geval van lets en where clauses moeten patterns van de buitenste expressie ook worden getypeerd.

--- master-thesis/contract-inferencing-helium/Helium.hs
+++ master-thesis/contract-inferencing-helium/Helium.hs
@@ -15,12 +15,12 @@
 module Domain.FP.Helium 
    ( -- * Compile functions
      compile, unsafeCompile, compile', compile_, compilePrelude
-   , compileWithExtraEnv, extractTypes, extractImportEnv, compile__
-   , compile''
+   , compileWithExtraEnv, extractTypes, extractImportEnv, compile__, compile___
+   , compile'', compile'''
      -- * Helium syntax data types
    , module Syntax.UHA_Syntax, module Syntax.UHA_Range
      -- * Pretty printing
-   , ppModule, ppBody, ppDeclaration, ppExpression, ppAlternative, ppFunctionBinding, ppPattern
+   , ppModule, ppBody, ppRhs, ppDeclaration, ppExpression, ppAlternative, ppFunctionBinding, ppPattern
      -- * Miscellaneous functions
    , patternVars, phaseDesugarer
    ) where
@@ -44,6 +44,7 @@ import System.IO.Unsafe (unsafePerformIO )
 import Main.CompileUtils hiding (doPhaseWithExit)
 import Control.Monad.Trans
 import StaticAnalysis.Messages.Warnings (Warning(Shadow))
+import qualified Text.PrettyPrint.Leijen as PPL
 
 import qualified Domain.FP.HeliumImportEnvs as H
 import Domain.FP.HeliumInstances ()
@@ -54,6 +55,9 @@ import Data.Maybe
 ppModule :: Module -> String
 ppModule m = show $
   PP.text_Syn_Module (PP.wrap_Module (PP.sem_Module m) PP.Inh_Module)
+ppRhs :: RightHandSide -> String
+ppRhs r = show $
+  PP.text_Syn_RightHandSide (PP.wrap_RightHandSide (PP.sem_RightHandSide r) PP.Inh_RightHandSide) PPL.empty
 ppBody :: Body -> String
 ppBody b = show $
   PP.text_Syn_Body (PP.wrap_Body (PP.sem_Body b) PP.Inh_Body)
@@ -238,7 +242,7 @@ compile__ isPrelude contents options extraEnv doneModules =
         let when :: Bool -> (a -> a) -> a -> a
             when p f a = if p then f a else a
             ns = toplevelNames parsedModule
-            importEnvs = when isPrelude (filterImportEnvs ns) H.importEnvs
+            importEnvs = when isPrelude (filterImportEnvs ns) (extraEnv:H.importEnvs)
         
         -- Phase 4: Resolving operators
         resolvedModule <- 
@@ -250,7 +254,73 @@ compile__ isPrelude contents options extraEnv doneModules =
             doPhaseWithExit $
                phaseStaticChecks fullName resolvedModule importEnvs options 
 
-        let localEnv' = combineImportEnvironments localEnv extraEnv        
+        let localEnv' = localEnv
+        
+        -- Phase 6: Kind inferencing (skipped)
+        let combinedEnv = foldr combineImportEnvironments localEnv' importEnvs
+        -- Phase 7: Type Inference Directives (skipped)
+        let beforeTypeInferEnv = combinedEnv
+        
+        -- Phase 8: Type inferencing
+        let newOptions = if null [() | Shadow _ _ <- staticWarnings]
+                            then options
+                            else NoOverloadingTypeCheck : options
+        (dictionaryEnv, afterTypeInferEnv, toplevelTypes, typeWarnings) <- 
+            doPhaseWithExit $ 
+               phaseTypeInferencer "." fullName resolvedModule localEnv' beforeTypeInferEnv newOptions
+        
+        return  (dictionaryEnv, afterTypeInferEnv, toplevelTypes, typeWarnings, resolvedModule)
+
+
+compile''' isPrelude txt extraEnv = unsafePerformIO $ do
+   ea <- run $ compile___ isPrelude txt [Overloading, UseTutor] extraEnv []
+   case ea of
+      Left ms -> return $ Left $ unlines ms
+      Right a -> return $ Right a 
+
+-- | Like compile_ but with an extra ImportEnvironment parameter.
+compile___ :: Bool -> String -> [Option] -> ImportEnvironment -> t
+         -> Compile ( DictionaryEnvironment
+                    , ImportEnvironment
+                    , TypeEnvironment
+                    , [Warning]
+                    , Module
+                    )
+compile___ isPrelude contents options extraEnv doneModules =
+    do
+        let fullName = "Prelude"
+        
+        -- Phase 1: Lexing
+        (lexerWarnings, tokens) <- 
+            doPhaseWithExit $
+               phaseLexer fullName contents options
+        
+        -- Phase 2: Parsing
+        parsedModule <- 
+            doPhaseWithExit $
+               phaseParser fullName tokens options
+        
+        -- Phase 3: Importing
+        -- We have a static import environment
+        -- (indirectionDecls, importEnvs) <-
+        --      liftIO $ phaseImport fullName parsedModule lvmPath options
+        
+        let when :: Bool -> (a -> a) -> a -> a
+            when p f a = if p then f a else a
+            ns = toplevelNames parsedModule
+            importEnvs = when isPrelude (filterImportEnvs ns) (extraEnv:H.importEnvs)
+        
+        -- Phase 4: Resolving operators
+        resolvedModule <- 
+            doPhaseWithExit $
+               phaseResolveOperators parsedModule importEnvs options
+        
+        -- Phase 5: Static checking
+        (localEnv, typeSignatures, staticWarnings) <-
+            doPhaseWithExit $
+               phaseStaticChecks fullName resolvedModule importEnvs options 
+
+        let localEnv' = localEnv
         
         -- Phase 6: Kind inferencing (skipped)
         let combinedEnv = foldr combineImportEnvironments localEnv' importEnvs
